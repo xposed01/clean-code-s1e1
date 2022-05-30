80e755ca8c52d42a6d6815994bf7b3f68500239a
xposed01 <43813249+xposed01@users.noreply.github.com>
2022-05-30 21:39:31 +0700

    fix: add alt text to img tag according to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="button delete";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className="btn-delete-img";
+    deleteButtonImg.alt="delete";
     deleteButton.appendChild(deleteButtonImg);
 
 
